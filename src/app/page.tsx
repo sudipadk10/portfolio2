48d983e6a237419a1cdc494c49abcdd6d61d894b
nischal1101 <kaflenk123@gmail.com>
2024-05-05 11:13:10 +0545

    only text remaining

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-
-import Home from './home/page';
-import Contact from './contact/page';
-import Skills from './skills/page';
-import About from './about/page';
-
+import Home from "./home/page";
+import Contact from "./contact/page";
+import Skills from "./skills/page";
+import Works from "./works/page";
+import About from "./about/page";
 
 export default function Ghar() {
   return (
-<div className="min-h-screen bg-[#282C34] flex flex-col font-fira-code">
-
-  <Home/>
-  <Skills/>
-  <About/>
-  <Contact/>
-    
- 
-</div>
+    <div className="min-h-screen bg-[#282C34] flex flex-col font-fira-code">
+      <Home />
+      <About />
+      <Skills />
+      <Works />
+      <Contact />
+    </div>
   );
 }
--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,11 +1,19 @@
 "use client";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 const ContactMe = () => {
-    const router=useRouter()
+  const router = useRouter();
   return (
-    <div className="mt-8 text-center md:text-left"><button onClick={()=>router.push('/contact')} className="inline-block border border-[#C778DE] py-2 px-4 text-white">Contact Me !!</button></div>
-  )
-}
+    <div className=" text-center md:text-left mt-8">
+      <button
+        onClick={() => router.push("/contact")}
+        className="inline-block border border-[#C778DE] py-2 px-4 text-white"
+      >
+        Contact Me !!
+      </button>
+      
+    </div>
+  );
+};
 
-export default ContactMe
+export default ContactMe;
